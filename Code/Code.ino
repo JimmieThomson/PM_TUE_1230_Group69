66da691036c0f0e06e4815b6714eeeecadefe6ff
James Thomson <101159512+JimmieThomson@users.noreply.github.com>
2022-09-09 00:30:04 +1000

    Another TEST

--- Code/Code.ino
+++ Code/Code.ino
@@ -1,10 +1,13 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.print("Test to see if pi reconises repo changes");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  int x;
+  while (x < 100){
+    Serial.println(x);
+    x++;
+  }
 }
